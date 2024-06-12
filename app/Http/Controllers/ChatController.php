80b5aabf43d03223debde3747a5cb9fb5602b653
Keisuke Karishuku <kkarishuku@gmail.com>
2024-06-12 13:43:12 +0800

    version 1.1

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -9,30 +9,7 @@ use App\Models\ChatMessage;
 
 class ChatController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
 
-
-    // public function index()
-    // {
-    //     $messages = ChatMessage::with('user')->get();
-    //     return response()->json($messages);
-    // }
-
-    // public function store(Request $request)
-    // {
-    //     $message = new ChatMessage([
-    //         'user_id' => auth()->user()->id,
-    //         'message' => $request->input('message'),
-    //     ]);
-    //     $message->save();
-
-    //     event(new NewMessageEvent($message));
-
-    //     return response()->json($message);
-    // }
 
     public function showPusherTest()
     {
